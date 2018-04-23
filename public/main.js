3c7cb6d4aab7611a331c8050c63415a3e287ff0c
aayush1408 <aayushjaiswal0114@gmail.com>
2018-04-23 20:40:19 +0530

    yourself move

--- public/main.js
+++ public/main.js
@@ -3,17 +3,21 @@ const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissor = document.getElementById('scissor');
 const opponent = document.getElementById('opponent');
+const yourself = document.getElementById('yourself');
 
 rock.addEventListener('click', () => {
-    socket.emit('rock', { data: 'Rock' })
+    socket.emit('rock', { data: 'Rock' });
+    yourself.innerHTML = 'You : Rock';
 });
 
 paper.addEventListener('click', () => {
-    socket.emit('paper', { data: 'Paper' })
+    socket.emit('paper', { data: 'Paper' });
+    yourself.innerHTML = 'You : Paper';
 });
 
 scissor.addEventListener('click', () => {
-    socket.emit('scissor', { data: 'Scissor' })
+    socket.emit('scissor', { data: 'Scissor' });
+    yourself.innerHTML = 'You : Scissor';
 });
 
 //Listen for events
