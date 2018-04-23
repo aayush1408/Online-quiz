79e0f23e2ca58d5bb0648ac9b5b9af3ef78f38e5
aayush1408 <aayushjaiswal0114@gmail.com>
2018-04-23 22:45:51 +0530

    results managed

--- public/main.js
+++ public/main.js
@@ -4,15 +4,19 @@ const paper = document.getElementById('paper');
 const scissor = document.getElementById('scissor');
 const opponent = document.getElementById('opponent');
 const yourself = document.getElementById('yourself');
+const winner = document.getElementById('result');
+var yourChoice = '';
 
 rock.addEventListener('click', () => {
     socket.emit('rock', { data: 'Rock' });
     yourself.innerHTML = 'You : Rock';
+    yourChoice = 'Rock'
 });
 
 paper.addEventListener('click', () => {
     socket.emit('paper', { data: 'Paper' });
     yourself.innerHTML = 'You : Paper';
+    yourChoice = 'Paper'
 });
 
 scissor.addEventListener('click', () => {
@@ -24,14 +28,31 @@ scissor.addEventListener('click', () => {
 socket.on('rock', (data) => {
     console.log(data);
     opponent.innerHTML = 'Opponent : ' + data.data;
+    result(data.data);
 });
 
 socket.on('scissor', (data) => {
     console.log(data);
     opponent.innerHTML = 'Opponent : ' + data.data;
+    result(data.data);
 });
 
 socket.on('paper', (data) => {
     console.log(data);
     opponent.innerHTML = 'Opponent : ' + data.data;
+    result(data.data);
 });
+
+function result(opponentValue) {
+    console.log(opponentValue);
+    console.log(yourChoice);
+    if (opponentValue === yourChoice) {
+        winner.innerHTML = 'No winner';
+    }
+    if ((opponentValue == 'Scissor' && yourChoice == 'Paper') || (opponentValue == 'Paper' && yourChoice == 'Rock') || (opponentValue == 'Rock' && yourChoice == 'Scissor')) {
+        winener.innerHTML = 'You Lost';
+    }
+    if ((opponentValue == 'Paper' && yourChoice == 'Scissor') || (opponentValue == 'Rock' && yourChoice == 'Paper') || (opponentValue == 'Scissor' && yourChoice == 'Rock')) {
+        winner.innerHTML = 'You won';
+    }
+}
